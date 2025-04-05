091d9697a2b46a3c4a6050097d7f070e3898e671
marcmarfer <marcmf2004@gmail.com>
2025-04-06 01:28:10 +0200

    Little padding change

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -20,7 +20,7 @@ const submit = () => {
     <Head title="Registro" />
 
     <div class="min-h-screen flex items-center justify-center bg-gray-100">
-        <div class="w-96">
+        <div class="w-96 p-4">
             <div class="text-center mb-8">
                 <h1 class="text-2xl font-bold text-gray-800">Neumatrans</h1>
                 <p class="text-gray-600 mt-2">Crea tu cuenta</p>
