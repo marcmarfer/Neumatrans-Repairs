ec2ae5bb89d8d709d2b85e3bd7c74f9bb68de488
marcmarfer <marcmf2004@gmail.com>
2025-07-19 16:14:11 +0200

    [Feature] Add a button to see the password

--- resources/js/Components/DefaultInput.vue
+++ resources/js/Components/DefaultInput.vue
@@ -62,31 +62,33 @@ function getInputClasses() {
     <label v-if="label" :for="id" class="block text-sm font-medium text-gray-700">
       {{ label }} <span v-if="required" class="text-red-500">*</span>
     </label>
+    <div class="relative">
+      <textarea
+        v-if="isTextarea"
+        :id="id"
+        :value="modelValue"
+        @input="updateValue"
+        :placeholder="placeholder"
+        :required="required"
+        :rows="rows"
+        :readonly="readonly"
+        :class="getInputClasses()"
+      ></textarea>
 
-    <textarea
-      v-if="isTextarea"
-      :id="id"
-      :value="modelValue"
-      @input="updateValue"
-      :placeholder="placeholder"
-      :required="required"
-      :rows="rows"
-      :readonly="readonly"
-      :class="getInputClasses()"
-    ></textarea>
+      <input
+        v-else
+        :id="id"
+        :type="type"
+        :value="modelValue"
+        @input="updateValue"
+        :placeholder="placeholder"
+        :required="required"
+        :readonly="readonly"
+        :class="getInputClasses()"
+      />
 
-    <input
-      v-else
-      :id="id"
-      :type="type"
-      :value="modelValue"
-      @input="updateValue"
-      :placeholder="placeholder"
-      :required="required"
-      :readonly="readonly"
-      :class="getInputClasses()"
-    />
-
+      <slot name="suffix"></slot>
+    </div>
     <div v-if="error" class="text-red-500 text-sm mt-1">{{ error }}</div>
   </div>
 </template>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { Head, Link, useForm } from '@inertiajs/vue3';
 import DefaultInput from '@/Components/DefaultInput.vue';
+import { ref } from 'vue';
 
 defineProps({
     canResetPassword: {
@@ -16,6 +17,7 @@ const form = useForm({
     password: '',
     remember: false,
 });
+const showPassword = ref(false);
 
 const submit = () => {
     form.post(route('login'), {
@@ -53,12 +55,24 @@ const submit = () => {
 
                 <DefaultInput
                     id="password"
-                    type="password"
+                    :type="showPassword ? 'text' : 'password'"
                     v-model="form.password"
                     placeholder="Contraseña"
                     required
                     :error="form.errors.password"
-                />
+                >
+                    <template #suffix>
+                        <button type="button" @click="showPassword = !showPassword" class="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500">
+                            <svg v-if="showPassword" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.269-2.943-9.543-7a9.958 9.958 0 012.083-3.23M6.343 6.343A9.955 9.955 0 0112 5c4.45 0 8.219 2.89 9.485 6.843M3 3l18 18"/>
+                            </svg>
+                            <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"/>
+                            </svg>
+                        </button>
+                    </template>
+                </DefaultInput>
 
                 <div class="flex items-center justify-between">
                     <label class="flex items-center">
